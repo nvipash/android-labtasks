b062cb12a114b8694ed1a073ed80b90003431c5c
Pavel Novitskyi <novipsh@outlook.com>
2018-09-03 01:10:48 +0300

    Laboratory work 1. Environment setup
    DoD:
    - IDE is set up and can compile your code
    - You can run an emulator on Android or simulator on iOS (latest stable version) from IDE
    - Run very basic app that prints "Hello World" into console
    - your .gitignore is properly setup
    - Your repo page on Github has proper README.md and license files (use MIT license)

---
+++ README.md
@@ -0,0 +1,2 @@
+# android_labtasks
+Android app for the "Programming of mobile apps" subject (V-th sem)
---
+++ app/src/main/java/iot/nvipash_app/MainActivity.java
@@ -0,0 +1,43 @@
+package iot.nvipash_app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        System.out.println("Hello World");
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "App by Pavlo Novitskiy", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+}
